95913c0da1bf7431fc8db3398a77499ce1c56da4
Caitlin <nightmaresandroses@live.nl>
2018-09-20 18:33:05 +0200

    header en bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,10 @@
     <title>@yield('title')</title>
     {{--yield is een placeholder, omdat dit de basis layout is verandert dit gebied voor elke pagina
     en dus passen we die per pagina aan--}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+    @include('includes.header')
     <div class="container">
         @yield('content')
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,5 +6,23 @@
 @endsection
 
 @section('content')
-    Dit is de content
+    <div class="row">
+        <div class="col-6">
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+            </form>
+
+        </div>
+    </div>
 @endsection
