e2de6f33a1e9be9819c4a5eceb401d93ea3b1e54
Caitlin <nightmaresandroses@live.nl>
2018-09-30 13:02:25 +0200

    View Welcome
    Sign Up en Sign In in de view toegevoegd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
 
 @section('content')
     <div class="row">
-        <div class="col-6">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
             <form action="#" method="post">
                 <div class="form-group">
                     <label for="email">Your E-Mail</label>
@@ -18,11 +19,25 @@
                     <input class="form-control" type="text" name="first_name" id="first_name">
                 </div>
                 <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
                     <label for="email">Your E-Mail</label>
                     <input class="form-control" type="text" name="email" id="email">
                 </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-
         </div>
     </div>
 @endsection
