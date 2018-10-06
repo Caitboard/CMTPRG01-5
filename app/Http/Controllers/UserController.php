001804a09dab85afca5fa6944dc184e95f79c1e5
Caitlin <nightmaresandroses@live.nl>
2018-10-06 14:56:42 +0200

    Login validatie
    User kan alleen inloggen als alle velden ingevuld zijn en het emailadres in email format is en niet al bestaat in de database. Naam max 50 characters.
    wachtwoord min 6 characters

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,16 @@ class UserController extends Controller
 
     public function postSignUp(Request $request)
     {
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+//            validating email => ' this field cannnot be empty | checking if the email really is an email address using
+//            laravels built-in feature | validating if this email does not already exist in the database table 'users' '
+            'first_name' => 'required|max:50',
+//            validating first name => 'checking if field is not empty | max characters of 50'
+            'password' => 'required|min:6'
+//            validating password => 'checking if field is not empty | minimum characters of 6'
+
+        ]);
         $email = $request['email'];
         $first_name = $request['first_name'];
         $password = bcrypt($request['password']);
@@ -27,6 +37,9 @@ class UserController extends Controller
 
         $user->save();
 //        Use save to write to the database
+
+        Auth::login($user);
+//      user logs in after signing up
         return redirect()->route('dashboard');
     }
     public function postSignIn(Request $request)
