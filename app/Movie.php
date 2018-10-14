d29eb385c02132405b79b49c9b04fce3f3a25497
Caitlin <nightmaresandroses@live.nl>
2018-10-14 17:43:53 +0200

    Update app/Movie.php

--- app/Movie.php
+++ app/Movie.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    public function()
+    public function user()
     {
         return $this->belongsTo('App\User');
 //        This post belongs to only one user, can't have multiple users
